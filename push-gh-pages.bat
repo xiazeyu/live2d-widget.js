ba8838cf565480193fb4a83d5e516a6164cc6664
xiazeyu_2011 <xiazeyu_2011@126.com>
2017-12-23 23:54:32 +0800

    The Real Initial Commit

---
+++ push-gh-pages.bat
@@ -0,0 +1,5 @@
+@echo off
+
+git subtree push --prefix=ghpages origin gh-pages
+
+pause
---
+++ src/index.js
@@ -0,0 +1,10 @@
+function component() {
+  var element = document.createElement('div');
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
