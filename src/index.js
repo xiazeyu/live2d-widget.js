f5f7043a02bdcc951faa0bf8e55e2911a4946369
xiazeyu_2011 <xiazeyu_2011@126.com>
2017-12-31 23:04:38 +0800

    feat(Webpack): updated webpack config

--- src/index.js
+++ src/index.js
@@ -4,25 +4,31 @@
 // Celebrate for the 3.0 version! //
 ////////////////////////////////////
 
+/**
+ * @description The entry file
+ */
+
 
 import device from 'current-device';
-// import config from './lib/configManager.js';
+import config from './lib/configManager.js';
 /**
  * The public entry point
  * @param  {Object} userConfig User's config
  */
 
-function loadL2D(userConfig){/*
-  config.checkConfig(userConfig);
+export function init(userConfig){
+  try{
+    config.checkConfig(userConfig);
+  }catch(err){
+    console.error(err);
+  }
   config.applyConfig(userConfig);
-  if((!config.mobileShow)&&(device.mobile())){
+  if((!config.options.mobile.show)&&(device.mobile())){
     return;
-  }*/
+  }
   import(/* webpackMode: "lazy" */ './cLive2DApp').then(_ => {
     _.default();
   }).catch(err => {
     console.error(err);
   });
 }
-
-window.loadL2D = loadL2D;
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const webpack = require('webpack');
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const visualizer = require('webpack-visualizer-plugin');
+const manifestPlugin = require('webpack-manifest-plugin');
+const nowDate = new Date();
+const isProd = e => e === 'prod';
+
+module.exports = env => {return{
+
+  entry: [
+    './src/index.js',
+  ],
+
+  output: {
+    filename: 'clL2D.min.js',
+    chunkFilename: 'clL2D.[id].min.js',
+    library: 'initL2Dwidget',
+    libraryExport: 'init',
+    libraryTarget: 'var',
+    path: path.resolve(__dirname, (isProd(env) ? 'lib' : 'dev')),
+    pathinfo: (isProd(env) ? false : true),
+  },
+
+  target: 'web',
+
+  devtool: 'source-map',
+
+  watch: (isProd(env) ? false : true),
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify((isProd(env) ? 'production' : 'development')),
+      }
+    }),
+    new UglifyJsPlugin({
+      cache: false,
+      parallel: true,
+      sourceMap: true,
+      uglifyOptions: {
+        // The L2D core library was droped too much,
+        // so the warnings is useless recently.
+        warnings: false,
+        mangle: true,
+        compress: {
+          drop_console: false,
+        },
+      },
+    }),
+    // Banner must be put below UglifyJsPlugin, or it won't work.
+    new webpack.BannerPlugin(`${isProd(env) ? '' : '___DEV___'}https://github.com/xiazeyu/live2d-widget.js built@${nowDate.toLocaleDateString()} ${nowDate.toLocaleTimeString()}`),
+    /**
+     * Webpack Manifest Plugin
+     * https://github.com/danethurber/webpack-manifest-plugin
+     */
+
+    new manifestPlugin(),
+    /**
+     * Webpack Visualizer
+     * https://github.com/chrisbateman/webpack-visualizer
+     */
+
+    new visualizer(),
+  ],
+
+  resolve: {
+    extensions: ['.js', '.webpack.js', '.web.js'],
+  },
+
+  module: {
+    rules: [
+      {test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        use: [{
+          loader: 'babel-loader',
+        }],
+      },
+      {test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: true,
+          },
+      }],
+      },
+    ]
+  },
+}}
