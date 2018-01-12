ec1b581c2ea71651e546deb86e37147380b13679
xiazeyu_2011 <xiazeyu_2011@126.com>
2018-01-12 22:12:48 +0800

    docs: fix bugs, update

--- src/index.js
+++ src/index.js
@@ -16,29 +16,29 @@ import { config, configApplyer }from './config/configMgr';
 
 /**
  * The public entry point
- * @param {Object}   [userConfig] User's custom config
- * @param {String}   [model.jsonPath = ''] Path to Live2D model's main json eg. https://test.com/miku.model.json
- * @param {Number}   [model.scale = 1] Scale between the model and the canvas
- * @param {Number}   [model.hHeadPos = 0.5] Horizontal position of model's head
- * @param {Number}   [model.vHeadPos = 0.618] Vertical position of model's head
- * @param {Array}    [model.myDefine = []] User's custom Defines which will override LDefine
- * @param {Number}   [display.AA = 2] Antialiasing grade
- * @param {Number}   [display.widget = 150] Widget to the canvas which shows the model
- * @param {Number}   [display.height = 300] Height to the canvas which shows the model
- * @param {String}   [display.position = 'right'] Left of right side to show
- * @param {Number}   [display.hOffset = 0] Horizontal offset of the canvas
- * @param {Number}   [display.vOffset = -20] Vertical offset of the canvas
- * @param {Boolean}  [mobile.show = true] Whether to show on mobile device
- * @param {Number}   [mobile.scale = 0.5] Scale on mobile device
- * @param {Boolean}  [mobile.motion = true] Whether to enable motion detection on mobile devices
- * @param {String}   [name.canvas = 'live2dcanvas'] ID name of the canvas
- * @param {String}   [name.div = 'live2d-widget'] ID name of the div
- * @param {Number}   [react.opacityDefault = 0.7] Default opacity
- * @param {Number}   [react.opacityOnHover = 0.2] OnHover opacity
- * @param {Function} [react.myFunc = func(e)] Custom event handler, won't override main handler, will reveice the event type.
- * @param {Boolean}  [dev.log = false] Whether to show log
- * @param {Boolean}  [dev.mouseLog = false] Whether to show mouse log (tons of log), only work when dev.log is enabled
- * @param {Function} [dev.mouseFunc = func(x, y, ix, iy)] Custom logger, only work when dev.log is enabled, will receive (x, y, ix, iy), which presents the actucally position and vitural position
+ * @param {Object}   [userConfig] User's custom config 用户自定义设置
+ * @param {String}   [userConfig.model.jsonPath = ''] Path to Live2D model's main json eg. `https://test.com/miku.model.json` model主文件路径
+ * @param {Number}   [userConfig.model.scale = 1] Scale between the model and the canvas 模型与canvas的缩放
+ * @param {Number}   [userConfig.model.hHeadPos = 0.5] Horizontal position of model's head 模型头部横坐标
+ * @param {Number}   [userConfig.model.vHeadPos = 0.618] Vertical position of model's head 模型头部纵坐标
+ * @param {Array}    [userConfig.model.myDefine = []] User's custom Defines which will override LDefine 自定义的LDefine
+ * @param {Number}   [userConfig.display.AA = 2] Antialiasing grade 抗锯齿
+ * @param {Number}   [userConfig.display.widget = 150] Widget to the canvas which shows the model canvas的长度
+ * @param {Number}   [userConfig.display.height = 300] Height to the canvas which shows the model canvas的高度
+ * @param {String}   [userConfig.display.position = 'right'] Left of right side to show 显示位置：左或右
+ * @param {Number}   [userConfig.display.hOffset = 0] Horizontal offset of the canvas canvas水平偏移
+ * @param {Number}   [userConfig.display.vOffset = -20] Vertical offset of the canvas canvas垂直偏移
+ * @param {Boolean}  [userConfig.mobile.show = true] Whether to show on mobile device 是否在移动设备上显示
+ * @param {Number}   [userConfig.mobile.scale = 0.5] Scale on mobile device 移动设备上的缩放
+ * @param {Boolean}  [userConfig.mobile.motion = true] Whether to enable motion detection on mobile devices 移动设备是否开启重力感应
+ * @param {String}   [userConfig.name.canvas = 'live2dcanvas'] ID name of the canvas canvas元素的ID
+ * @param {String}   [userConfig.name.div = 'live2d-widget'] ID name of the div div元素的ID
+ * @param {Number}   [userConfig.react.opacityDefault = 0.7] Default opacity 默认透明度
+ * @param {Number}   [userConfig.react.opacityOnHover = 0.2] OnHover opacity 鼠标移上透明度
+ * @param {Function} [userConfig.react.myFunc = func(e)] Custom event handler, won't override main handler, will reveice the event type. 自定义事件接收器
+ * @param {Boolean}  [userConfig.dev.log = false] Whether to show log 显示日志
+ * @param {Boolean}  [userConfig.dev.mouseLog = false] Whether to show mouse log (tons of log), only work when dev.log is enabled 显示鼠标移动
+ * @param {Function} [userConfig.dev.mouseFunc = func(x, y, ix, iy)] Custom logger, only work when dev.log is enabled, will receive (x, y, ix, iy), which presents the actucally position and vitural position 自定义鼠标移动处理函数
  */
 
 function init(userConfig){
