d30a5d61b566d149bdfd2460fab517c5e3a944c1
xiazeyu <xiazeyu_2011@126.com>
2018-03-23 23:16:28 +0800

    ci: fix secure errors for prs

---
+++ .travis/deploy_docs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+openssl aes-256-cbc -K $encrypted_7ae5308e2fd2_key -iv $encrypted_7ae5308e2fd2_iv -in .travis/id_rsa.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_rsa
+cp .travis/ssh_config ~/.ssh/config
+cp .travis/ssh_config ~/.ssh/config
+git config --global user.name 'xiazeyu'
+git config --global user.email 'xiazeyu_2011@126.com'
+npm run build:esdoc
+npm run deploy:ghpages
