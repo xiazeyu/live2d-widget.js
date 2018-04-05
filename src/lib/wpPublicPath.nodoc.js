a4228fb665e5cf9458c8fa4f6b690cfb431867ce
xiazeyu_2011 <xiazeyu_2011@126.com>
2018-04-05 22:13:46 +0800

    ci: move compiled folder from lib to dist
    BREAKING CHANGE: Please use /dist/L2Dwidget.min.js instead of using /lib/L2Dwidget.min.js

---
+++ src/lib/wpPublicPath.nodoc.js
@@ -0,0 +1,13 @@
+/* global __webpack_public_path__: true, process */
+
+import { getPathFromUrl } from '../utils/getPathFromUrl';
+
+// Expose the path to the global,
+// And wp will finish the following work
+__webpack_public_path__ = getPathFromUrl(document.currentScript.src); // eslint-disable-line camelcase
+
+if (process.env.NODE_ENV === 'development') {
+
+  console.log(`Live2Dwidget: publicPath: ${__webpack_public_path__}`); // eslint-disable-line camelcase
+
+}
