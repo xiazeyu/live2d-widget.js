8927fcfc7b5f8c839a73d0b52467f6b4c448fc2f
xiazeyu <xiazeyu_2011@126.com>
2018-05-19 19:12:37 +0800

    ci: organize file structure

--- build/deployDocs.sh
+++ build/deployDocs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-openssl aes-256-cbc -K $encrypted_7ae5308e2fd2_key -iv $encrypted_7ae5308e2fd2_iv -in .travis/id_rsa.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $encrypted_7ae5308e2fd2_key -iv $encrypted_7ae5308e2fd2_iv -in build/id_rsa.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 eval $(ssh-agent)
 ssh-add ~/.ssh/id_rsa
-cp .travis/ssh_config ~/.ssh/config
-cp .travis/ssh_config ~/.ssh/config
+cp build/ssh_config ~/.ssh/config
 git config --global user.name 'xiazeyu'
 git config --global user.email 'xiazeyu_2011@126.com'
 npm run build:esdoc
