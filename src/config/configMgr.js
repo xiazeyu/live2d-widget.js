9482e8342ae423023bcf5a020df0d1067bf0668b
xiazeyu <xiazeyu_2011@126.com>
2018-06-09 22:00:38 +0800

    feat(config): configUpdater, new API configUpdater: for downward compatibility new API(think to be fixed @4)
    BREAKING CHANGES: new API.
    fix #29, re #19

--- src/config/configMgr.js
+++ src/config/configMgr.js
@@ -1,45 +1,309 @@
 import configStorage from './configStorage';
 
+/**
+ * To update a Config from previous version.
+ * @param {Config} oldConfig  Any previous version of Config.
+ * @return {Config}  Updated Config.
+ */
+function configUpdater (oldConfig) {
+
+  const newConfig = {};
+  // For live2d-widget@3.0.0 to live2d-widget@3.0.5
+  if ('model' in oldConfig) {
+
+    if ('jsonPath' in oldConfig.model) {
+
+      newConfig.modelJsonPath = oldConfig.model.jsonPath;
+
+    }
+    if ('scale' in oldConfig.model) {
+
+      console.error('model.scale: deprecated.');
+
+    }
+    if ('hHeadPos' in oldConfig.model) {
+
+      newConfig.modelHeadPosH = oldConfig.model.hHeadPos;
+
+    }
+    if ('vHeadPos' in oldConfig.model) {
+
+      newConfig.modelHeadPosV = oldConfig.model.vHeadPos;
+
+    }
+    if ('myDefine' in oldConfig.model) {
+
+      console.error('model.myDefine: deprecated.');
+
+    }
+
+  }
+  if ('display' in oldConfig) {
+
+    if ('superSample' in oldConfig.display) {
+
+      newConfig.displaySampleLevel = oldConfig.display.superSample;
+
+    }
+    if ('width' in oldConfig.display) {
+
+      newConfig.displayWidth = oldConfig.display.width;
+
+    }
+    if ('height' in oldConfig.display) {
+
+      newConfig.displayHeight = oldConfig.display.height;
+
+    }
+
+    if ('position' in oldConfig.display) {
+
+      newConfig.displaySide = oldConfig.display.position;
+
+    }
+
+    if ('hOffset' in oldConfig.display) {
+
+      newConfig.displayOffsetH = oldConfig.display.hOffset;
+
+    }
+
+    if ('vOffset' in oldConfig.display) {
+
+      newConfig.displayOffsetV = oldConfig.display.vOffset;
+
+    }
+
+  }
+  if ('mobile' in oldConfig) {
+
+    if ('show' in oldConfig.mobile) {
+
+      console.error('mobile.show: please use displayFunc manually.');
+
+    }
+    if ('scale' in oldConfig.mobile) {
+
+      console.error('mobile.scale: please use displayFunc manually.');
+
+    }
+    if ('motion' in oldConfig.mobile) {
+
+      newConfig.displayDeviceMotion = oldConfig.mobile.motion;
+
+    }
+
+  }
+
+  if ('name' in oldConfig) {
+
+    if ('canvas' in oldConfig.name) {
+
+      console.error('name.canvas: deprecated.');
+
+    }
+    if ('div' in oldConfig.name) {
+
+      console.error('name.div: deprecated');
+
+    }
+
+  }
+
+  if ('react' in oldConfig) {
+
+    if ('opacityDefault' in oldConfig.react) {
+
+      newConfig.displayOpacityDefault = oldConfig.react.opacityDefault;
+
+    }
+    if ('opacityOnHover' in oldConfig.react) {
+
+      newConfig.displayOpacityOnHover = oldConfig.react.opacityOnHover;
+
+    }
+    if ('myFunc' in oldConfig.react) {
+
+      console.error('react.myFunc: deprecated');
+
+    }
+
+  }
+  if ('dev' in oldConfig) {
+
+    if ('log' in oldConfig.dev) {
+
+      newConfig.devLog = oldConfig.dev.log;
+
+    }
+    if ('border' in oldConfig.dev) {
+
+      newConfig.devBorder = oldConfig.dev.border;
+
+    }
+    if ('mouseLog' in oldConfig.dev) {
+
+      newConfig.devMouseLog = oldConfig.dev.mouseLog;
+
+    }
+    if ('mouseFunc' in oldConfig.dev) {
+
+      console.error('dev.mouseFunc: deprecated.');
+
+    }
+
+  }
+
+  // For hexo-helper-live2d@2.0.0 to hexo-helper-live2d@2.1.5 (config only)
+  if ('model' in oldConfig) {
+
+    console.error('model: use modelJsonPath instead.');
+
+  }
+  if ('width' in oldConfig) {
+
+    newConfig.displayWidth = oldConfig.width;
+
+  }
+  if ('height' in oldConfig) {
+
+    newConfig.displayHeight = oldConfig.height;
+
+  }
+  if ('scaling' in oldConfig) {
+
+    newConfig.displaySampleLevel = oldConfig.scaling;
+
+  }
+  if ('opacityDefault' in oldConfig) {
+
+    newConfig.displayOpacityDefault = oldConfig.opacityDefault;
+
+  }
+  if ('opacityHover' in oldConfig) {
+
+    newConfig.displayOpacityOnHover = oldConfig.opacityHover;
+
+  }
+  if ('mobileShow' in oldConfig) {
+
+    console.error('mobileShow: please use displayFunc manually.');
+
+  }
+  if ('mobileScaling' in oldConfig) {
+
+    console.error('mobileScaling: please use displayFunc manually.');
+
+  }
+  if ('position' in oldConfig) {
+
+    newConfig.displaySide = oldConfig.position;
+
+  }
+  if ('horizontalOffset' in oldConfig) {
+
+    newConfig.displayOffsetH = oldConfig.horizontalOffset;
+
+  }
+  if ('verticalOffset' in oldConfig) {
+
+    newConfig.displayOffsetV = oldConfig.verticalOffset;
+
+  }
+  if ('className' in oldConfig) {
+
+    console.error('className: deprecated.');
+
+  }
+  if ('id' in oldConfig) {
+
+    console.error('id: deprecated.');
+
+  }
+  if ('bottom' in oldConfig) {
+
+    newConfig.displayOffsetV = oldConfig.bottom;
+
+  }
+
+  // Convert type
+
+  if ('displayHeight' in newConfig) {
+
+    if (typeof newConfig.displayHeight !== 'string') {
+
+      newConfig.displayHeight = `${newConfig.displayHeight}px`;
+
+    }
+
+  }
+  if ('displayWidth' in newConfig) {
+
+    if (typeof newConfig.displayWidth !== 'string') {
+
+      newConfig.displayWidth = `${newConfig.displayWidth}px`;
+
+    }
+
+  }
+  if ('displayOffsetH' in newConfig) {
+
+    if (typeof newConfig.displayOffsetH !== 'string') {
+
+      newConfig.displayOffsetH = `${newConfig.displayOffsetH}px`;
+
+    }
+
+  }
+  if ('displayOffsetV' in newConfig) {
+
+    if (typeof newConfig.displayOffsetV !== 'string') {
+
+      newConfig.displayOffsetV = `${newConfig.displayOffsetV}px`;
+
+    }
+
+  }
+
+
+  return newConfig;
+
+}
 
 /**
- * Default config according to user's config and default config.
+ * Default config based on user's config and default config.<br>
+ * Will update the Config first.
  * @param  {Config} userConfig  User's custom config.
  * @return {Config}             Config that has been defaulted.
  */
 function configDefaulter (userConfig) {
 
-  if (typeof userConfig.displayHeight === 'number') {
+  const newConfig = configUpdater(userConfig);
 
-    userConfig.displayHeight = `${userConfig.displayHeight}px`;
-
-  }
-
-  if (typeof userConfig.displayWidth === 'number') {
-
-    userConfig.displayWidth = `${userConfig.displayWidth}px`;
-
-  }
-
-  return Object.assign({}, userConfig, configStorage.defaultConfig);
+  return Object.assign({}, newConfig, configStorage.defaultConfig);
 
 }
 
 
 
 /**
- * To validate whether user config is valid.
- * @param  {Config} userConfig User's custom config.
- * @return {Boolean}           Whether the config is valid.
+ * To validate whether user config is valid.<br>
+ * Console an error if there's something wrong.
+ * @param  {Config} userConfig  User's custom config.
+ * @return {Boolean}            Whether the config is valid.
  */
 function configValidater (userConfig) {
+
+  let validFlag = true;
 
   for (const key in configStorage.configType) {
 
     if ({}.hasOwnProperty.call(configStorage.configType, key)) {
 
-      if (typeof userConfig[key] !== configStorage.configType[key]) {
+      if (key in userConfig && typeof userConfig[key] !== configStorage.configType[key]) {
 
-        throw new Error(`config.${key} is expected a ${configStorage.configType[key]}, but got a ${typeof userConfig[key]}`);
+        validFlag = false;
+        console.error(`config.${key} is expected a ${configStorage.configType[key]}, but got a ${typeof userConfig[key]}`);
 
       }
 
@@ -48,11 +312,12 @@ function configValidater (userConfig) {
 
   }
 
-  return true;
+  return validFlag;
 
 }
 
 export {
   configDefaulter,
   configValidater,
+  configUpdater,
 };
--- src/config/configStorage.js
+++ src/config/configStorage.js
@@ -1,80 +1,123 @@
 
-function sampleDisplayFunc() {
+/* global device */
+
+/**
+ * An example of Config.displayFunc, to manually get display config on the basis of current client browsers.
+ * <br>[current-device](https://github.com/matthewhudson/current-device) can be used to detect current device.
+ * <br>Whatever key returns `null` means nothing to do with this key, and it will keep it's original value.
+ * @return {Config}  A Config that specifies the display config.
+ */
+function sampleDisplayFunc () {
+
+  /**
+   * To specifies the proper sample level on the basis of current device.
+   * @return {Number}  Sample level.
+   */
+  function autoSampleLevel () { // eslint-disable-line no-unused-vars
+
+    if (!device.mobile()) {
+
+      return 2; // eslint-disable-line no-magic-numbers
+
+    }
+    return 1; // eslint-disable-line no-magic-numbers
+
+  }
+
   return {
+    'displayDeviceMotion': null,
     'displayHeight': null,
-    'displayMotion': null,
     'displayOffsetH': null,
     'displayOffsetV': null,
     'displayOpacityDefault': null,
     'displayOpacityOnHover': null,
-    'displaySampleLevel': null,
-    'displayShow': null,
+    'displaySampleLevel': null/* autoSampleLevel() */, // eslint-disable-line capitalized-comments
+    'displayScale': null,
+    'displayShow': null/* !device.mobile() */, // eslint-disable-line capitalized-comments
     'displaySide': null,
     'displayWidth': null,
   };
+
 }
 
 /**
- * @typedef {Object} Config
- * @desc The object structure for config. Default config sees [dist/configStorage.js](https://cdn.jsdelivr.net/npm/live2d-widget/dist/configStorage.js).
- * @property {String}   modelJsonPath="https://cdn.jsdelivr.net/npm/live2d-widget-model-shizuku/assets/shizuku.model.json"  Specifies the path to `.model.json`.  模型`.model.json`文件路径 <br> eg. `"/live2d/miku.model.json"`<br>`"https://test.com/live2d/miku.model.json"`<br>`"http://test.com/live2d/miku.model.json"`<br>`"file:///d:/live2d/miku.model.json"`
- * @property {String}   displayWidth="150px"         Specifies the width of actual widget element.  插件宽度 <br> eg. `150` == `"150px"`, `"35%"`, `"auto"` <br> *used as canvas.style.width.*
- * @property {String}   displaySide="right"          Specifies left or right side to show.  左右侧显示位置 <br> eg. `"left"`, `"right"`
- * @property {String}   displayHeight="300px"        Specifies the height of actual widget element.  插件高度 <br> eg. `300` == `"300px"`, `"35%"`, `"auto"` <br> *used as canvas.style.height.*
- * @property {Number}   reactOpacityOnHover  OnHover opacity.  鼠标移上透明度
- * @property {Number}   reactOpacityDefault  Default opacity.  默认透明度
- * @property {Number}   modelScale           Scale rate between model and canvas.  画布所占缩放比例
- * @property {Number}   modelHeadPosV        Vertical position of model's head.  模型头部纵坐标
- * @property {Number}   modelHeadPosH        Horizontal position of model's head.  模型头部横坐标
- * @property {Number}   mobileScale          Scale rate on mobile device.  移动设备上缩放比例
- * @property {Number}   displaySampleLevel   Sampling level.  采样等级
- * @property {Number}   displayOffsetV       Vertical offset of canvas.  canvas垂直偏移
- * @property {Number}   displayOffsetH       Horizontal offset of canvas.  canvas水平偏移
- * @property {Boolean}  mobileMotion         If enable motion reaction on mobile devices.  是否在移动设备上开启运动响应
- * @property {Boolean}  devMouseLog=false          If shows mouse log.  显示鼠标日志
- * @property {Boolean}  devLog=false               If shows log.  显示日志
- */
+ * Structure for config, {@link defaultConfig} as the default, {@link configType} as the type.
+ * @typedef {Object}  Config
+ * @property {string}  modelJsonPath="https://cdn.jsdelivr.net/npm/live2d-widget-model-shizuku/assets/shizuku.model.json"    Specifies the `.model.json` file path of model.<br>指定模型的文件`.model.json`位置。<br>eg. `"/live2d/miku.model.json"`,<br>`"https://test.com/live2d/miku.model.json"`,<br>`"http://test.com/live2d/miku.model.json"`,<br>`"file:///d:/live2d/miku.model.json"`.
+ * @property {number}  modelAspectRatio=0.75    [BETA]Specifies the aspect ratio of the model in order to prevent being deformed.<br>指定模型的宽高比以防止被拉伸失真。<br>**Only works when provided size type is "dynamic"**<br>****仅在大小类型为动态时被启用。****
+ * @property {number}  modelHeadPosH=0.5    Specifies the head's horizontal position of the model.<br>指定模型的水平头部位置。
+ * @property {number}  modelHeadPosV=0.618    Specifies the head's vertical position of the model.<br>指定模型的竖直头部位置。
+ * @property {boolean}  displayShow=true    Specifies whether to show live2d-widget element.<br>指定是否显示live2d-widget元素。
+ * @property {string}  displaySide="right"    Specifies which side to show live2d-widget element.<br>指定何侧显示live2d-widget元素。<br>eg. `right`, `left`.
+ * @property {string}  displayHeight="45%"    [BETA]Specifies the height of live2d-widget element.<br>指定live2d-widget元素的高度。<br>There's two **size types**:<br>有2种大小类型。<br>a. "fixed" ones,<br>固定的。<br>eg.`300px` or `300`(will be processed as `300px`, just for compatible).<br>b. "dynamic" ones*[BETA]*<br>动态的。<br>eg. `45%`**This only works when target window is LANDSCAPE**.<br>**仅在您的目标窗口为横屏时被使用**。
+ * @property {string}  displayWidth="35%"    [BETA]Specifies the width of live2d-widget element.<br>指定live2d-widget元素的宽度。<br>There's two **size types**:<br>有2种大小类型。<br>a. "fixed" ones,<br>固定的。<br>eg.`150px` or `150`(will be processed as `150px`, just for compatible).<br>b. "dynamic" ones*[BETA]*<br>动态的。<br>eg. `35%`**This only works when target window is PORTRAIT**.<br>**仅在您的目标窗口为竖屏时被使用**。
+ * @property {string}  displayOffsetH="0px"    Specifies the horizontal offset of live2d-widget element.<br>指定live2d-widget元素自侧部的水平偏移。
+ * @property {string}  displayOffsetV="-20px"    Specifies the vertical offset of live2d-widget element.<br>指定live2d-widget元素自底部的垂直偏移。
+ * @property {number}  displaySampleLevel=2    [INFO]Specifies the sample level of the canvas.<br>指定画布的超采样级别。<br>lower is faster, but blur,<br>数值越小越快速，也越模糊。<br>higher is distinct, but slower.<br>数值越大越清晰，也越缓慢。
+ * @property {function}  displayFunc=sampleDisplayFunc    [BETA]Specifies which function will be used to override display configs at startup.<br>指定用于在启动时用于覆写display设置项的函数。<br>Sees {@link sampleDisplayFunc} as an example.<br>参照样例{@link sampleDisplayFunc}。
+ * @property {number}  displayOpacityDefault=0.7    Specifies the opacity of live2d-widget element.<br>指定live2d-widget元素的透明度。
+ * @property {number}  displayOpacityOnHover=0.2    Specifies the opacity when mouse is on hover the element.<br>指定live2d-widget元素鼠标上移后的透明度。
+ * @property {boolean}  displayDeviceMotion=true    [WIP]Specifies whether to use device motion.<br>指定是否使用设备动作感应。
+ * @property {number}  zindex=2333    Specifies the z-order of live2d-widget element.<br>指定模型的z-index数值。
+ * @property {boolean}  devBorder=false    [DEV]Specifies whether to enable border around the canvas for debugging.<br>指定是否启用调试用描边。
+ * @property {boolean}  devLog=false    [DEV]Specifies whether to enable log in console for debugging.<br>指定是否启用调试用日志。
+ * @property {boolean}  devMouseLog=false    [DEV][WARN]Specifies whether to enable mouse log in console for debugging.<br>指定是否启用调试用鼠标日志。<br>NOTICE: may cause lag.<br>可能造成卡顿。
+ * */
 
+/**
+ * Default to live2d-widget.js's config.
+ * @type {Config}
+ */
 const defaultConfig = {
+  'devBorder': false,
   'devLog': false,
   'devMouseLog': false,
+  'displayDeviceMotion': true,
   'displayFunc': sampleDisplayFunc,
-  'displayHeight': '300px',
-  'displayMotion': true,
-  'displayOffsetH': 0,
-  'displayOffsetV': -20,
+  'displayHeight': '45%',
+  'displayOffsetH': '0px',
+  'displayOffsetV': '-20px',
   'displayOpacityDefault': 0.7,
   'displayOpacityOnHover': 0.2,
   'displaySampleLevel': 2,
   'displayShow': true,
   'displaySide': 'right',
-  'displayWidth': '150px',
+  'displayWidth': '35%',
+  'modelAspectRatio': 0.75,
   'modelHeadPosH': 0.5,
   'modelHeadPosV': 0.618,
   'modelJsonPath': 'https://cdn.jsdelivr.net/npm/live2d-widget-model-shizuku/assets/shizuku.model.json',
-  'z-index': 2333,
-}
+  'zindex': 2333,
+};
 
+/**
+ * Type of {@link Config}, only compatible with the latest Config.
+ * <br>Use {@link configUpdater} if you are using previous version of Config.
+ */
 const configType = {
+  'devBorder': 'boolean',
   'devLog': 'boolean',
   'devMouseLog': 'boolean',
+  'displayDeviceMotion': 'boolean',
+  'displayFunc': 'function',
   'displayHeight': 'string',
-  'displayOffsetH': 'number',
-  'displayOffsetV': 'number',
+  'displayOffsetH': 'string',
+  'displayOffsetV': 'string',
+  'displayOpacityDefault': 'number',
+  'displayOpacityOnHover': 'number',
   'displaySampleLevel': 'number',
+  'displayShow': 'boolean',
   'displaySide': 'string',
   'displayWidth': 'string',
-  'mobileMotion': 'boolean',
-  'mobileScale': 'number',
-  'mobileShow': 'boolean',
+  'modelAspectRatio': 'number',
   'modelHeadPosH': 'number',
   'modelHeadPosV': 'number',
   'modelJsonPath': 'string',
-  'modelScale': 'number',
-  'reactOpacityDefault': 'number',
-  'reactOpacityOnHover': 'number',
+  'zindex': 'number',
 };
 
 export {
+  configType,
   defaultConfig,
+  sampleDisplayFunc,
 };
