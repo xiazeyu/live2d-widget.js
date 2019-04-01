e79f57c2f5f80eb2394b03ae813926087da8785b
19890913aa <cneyhn@gmail.com>
2019-04-01 15:31:36 +0800

    fix: canvas size error

--- src/elementMgr.js
+++ src/elementMgr.js
@@ -25,30 +25,30 @@ let currCanvas;
  * @return {null}
  */
 
-function createElement(){
+function createElement() {
 
   let e = document.getElementById(config.name.div)
-  if (e !== null){
+  if (e !== null) {
     document.body.removeChild(e);
   }
 
   let newElem = document.createElement('div');
   newElem.id = config.name.div;
   newElem.innerHTML = htmlTemplate;
-    let newCanvasElem = document.createElement('canvas');
-    newCanvasElem.setAttribute('id', config.name.canvas);
-    newCanvasElem.setAttribute('width', config.display.width * config.display.superSample);
-    newCanvasElem.setAttribute('height', config.display.height * config.display.superSample);
-    newCanvasElem.style.setProperty('position', 'fixed');
-    newCanvasElem.style.setProperty('width', config.display.width);
-    newCanvasElem.style.setProperty('height', config.display.height);
-    newCanvasElem.style.setProperty('opacity', config.react.opacityDefault);
-    newCanvasElem.style.setProperty(config.display.position, config.display.hOffset + 'px');
-    newCanvasElem.style.setProperty('bottom', config.display.vOffset + 'px');
-    newCanvasElem.style.setProperty('z-index', 99999);
-    newCanvasElem.style.setProperty('pointer-events', 'none');
-    if(config.dev.border) newCanvasElem.style.setProperty('border', 'dashed 1px #CCC');
-    newElem.appendChild(newCanvasElem);
+  let newCanvasElem = document.createElement('canvas');
+  newCanvasElem.setAttribute('id', config.name.canvas);
+  newCanvasElem.setAttribute('width', config.display.width * config.display.superSample);
+  newCanvasElem.setAttribute('height', config.display.height * config.display.superSample);
+  newCanvasElem.style.setProperty('position', 'fixed');
+  newCanvasElem.style.setProperty('width', config.display.width + 'px');
+  newCanvasElem.style.setProperty('height', config.display.height + 'px');
+  newCanvasElem.style.setProperty('opacity', config.react.opacityDefault);
+  newCanvasElem.style.setProperty(config.display.position, config.display.hOffset + 'px');
+  newCanvasElem.style.setProperty('bottom', config.display.vOffset + 'px');
+  newCanvasElem.style.setProperty('z-index', 99999);
+  newCanvasElem.style.setProperty('pointer-events', 'none');
+  if (config.dev.border) newCanvasElem.style.setProperty('border', 'dashed 1px #CCC');
+  newElem.appendChild(newCanvasElem);
 
   document.body.appendChild(newElem);
   currCanvas = document.getElementById(config.name.canvas);
@@ -62,23 +62,23 @@ function createElement(){
  * @return {null}
  */
 
-function initWebGL(){
+function initWebGL() {
 
   var NAMES = ['webgl2', 'webgl', 'experimental-webgl2', 'experimental-webgl', 'webkit-3d', 'moz-webgl'];
-  for(let i = 0; i < NAMES.length; i++){
-    try{
+  for (let i = 0; i < NAMES.length; i++) {
+    try {
       let ctx = currCanvas.getContext(NAMES[i], {
         alpha: true,
         antialias: true,
         premultipliedAlpha: true,
         failIfMajorPerformanceCaveat: false,
       });
-      if(ctx) currWebGL = ctx;
-    }catch(e){}
+      if (ctx) currWebGL = ctx;
+    } catch (e) { }
   }
-  if(!currWebGL){
+  if (!currWebGL) {
     console.error('Live2D widgets: Failed to create WebGL context.');
-    if(!window.WebGLRenderingContext){
+    if (!window.WebGLRenderingContext) {
       console.error('Your browser may not support WebGL, check https://get.webgl.org/ for futher information.');
     }
     return;
@@ -86,7 +86,7 @@ function initWebGL(){
 };
 
 
-export{
+export {
   createElement,
   currWebGL,
   currCanvas,
